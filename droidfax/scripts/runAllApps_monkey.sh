ac3d7dc48009360c44f2f6f27f2ac6fa9ff1b534
Ismael Medeiros <ismaelcmedeiros@gmail.com>
2020-04-17 02:49:07 -0300

    [GH-3] Improved code on runAllApps_monkey..

--- droidfax/scripts/runAllApps_monkey.sh
+++ droidfax/scripts/runAllApps_monkey.sh
@@ -36,8 +36,10 @@ runsingle()
 	./apkmng/apkinstall $srcdir/$i/$x.apk
 	adb logcat -v raw -s "hcai-intent-monitor" "hcai-cg-monitor" &>$OUTDIR/${i}-$x.logcat &
 	tgtp=`$TOOLHOME/apkmng/getpackage.sh $srcdir/$i/$x.apk | awk '{print $2}'`
-	timeout $tmv "adb shell monkey -p $tgtp --ignore-crashes --ignore-timeouts --ignore-security-exceptions --throttle 200 10000000 >$OUTDIR/${i}-$x.monkey"
+	# timeout $tmv "adb shell monkey -p $tgtp --ignore-crashes --ignore-timeouts --ignore-security-exceptions --throttle 200 10000000 >$OUTDIR/${i}-$x.monkey"
+	timeout $tmv "adb shell monkey -p $tgtp --ignore-security-exceptions 100000 >$OUTDIR/${i}-$x.monkey"
 	./apkmng/apkuninstall $srcdir/$i/$x.apk
+	# adb shell ps | awk '/com\.android\.commands\.monkey/ { system("adb shell kill " $2) }' # Kill monkey
 	killall -9 adb
 }
 
