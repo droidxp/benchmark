bfdd936ce656f4f076e4714b899530bcd08ed193
Ismael Medeiros <ismaelcmedeiros@gmail.com>
2020-04-25 00:31:21 -0300

    [GH-4] Changed phase two to use new instrumented files.

--- droidfax/scripts/runAllApps_monkey.sh
+++ droidfax/scripts/runAllApps_monkey.sh
@@ -28,24 +28,23 @@ timeout() {
 
 runsingle()
 {
-	i=$1
-	x=$2
-	if [ ! -s $srcdir/${i}/$x.apk ];then return; fi
+	file=$1
+	if [ ! -s $srcdir/$file ];then return; fi
 	echo "Starting android emulator ......"
 	$TOOLHOME/setupEmu.sh Nexus-One-10 2>/dev/null 1>&2
-	./apkmng/apkinstall $srcdir/$i/$x.apk
-	adb logcat -v raw -s "hcai-intent-monitor" "hcai-cg-monitor" &>$OUTDIR/${i}-$x.logcat &
-	tgtp=`$TOOLHOME/apkmng/getpackage.sh $srcdir/$i/$x.apk | awk '{print $2}'`
+	./apkmng/apkinstall $srcdir/$file
+	adb logcat -v raw -s "hcai-intent-monitor" "hcai-cg-monitor" &>$OUTDIR/$file.logcat &
+	tgtp=`$TOOLHOME/apkmng/getpackage.sh $srcdir/$file | awk '{print $2}'`
 	# timeout $tmv "adb shell monkey -p $tgtp --ignore-crashes --ignore-timeouts --ignore-security-exceptions --throttle 200 10000000 >$OUTDIR/${i}-$x.monkey"
-	timeout $tmv "adb shell monkey -p $tgtp --ignore-security-exceptions 100000 >$OUTDIR/${i}-$x.monkey"
-	./apkmng/apkuninstall $srcdir/$i/$x.apk
+	timeout $tmv "adb shell monkey -p $tgtp --ignore-security-exceptions 100000 >$OUTDIR/$file.monkey"
+	./apkmng/apkuninstall $srcdir/$file
 	# adb shell ps | awk '/com\.android\.commands\.monkey/ { system("adb shell kill " $2) }' # Kill monkey
 	killall -9 adb
 }
 
-for ((i=1;i<=$APPPAIRNUM;i++))
+for file in `ls $srcdir`
 do
-	runsingle $i s
+	runsingle $file
 done
 
 exit 0
