1ec452249dd8ef6908000411895aa24e180aabb0
Thales Menezes <thaleslim@localhost.localdomain>
2020-06-18 19:28:14 -0300

    fix: droidmate tool execution bug

--- tools/droidmate/tool.py
+++ tools/droidmate/tool.py
@@ -10,7 +10,7 @@ from ..tool_spec import AbstractTool
 class ToolSpec(AbstractTool):
     def __init__(self):
         super(ToolSpec, self).__init__("droidmate", """DroidMate-2 is a platform to easily assist both developers
-        and researchers to customize, develop and test new test generators.(https://github.com/uds-se/droidmate).""", 'com.android.commands.droidmate')
+        and researchers to customize, develop and test new test generators.(https://github.com/uds-se/droidmate).""", 'org.droidmate')
 
     def execute_tool_specific_logic(self, fileName, timeout):
         trace_file = os.path.join(TRACE_DIR, self.name, fileName + "." + self.name)
@@ -21,8 +21,6 @@ class ToolSpec(AbstractTool):
             exec_cmd = Command('java',[
                 '-jar',
                 '{}'.format(droidmate_jar),
-                '--Core-logLevel=info',
-                '--Selectors-timeLimit=10',
                 '--Exploration-apkNames={0}'.format(fileName),
                 '--Exploration-apksDir={0}'.format(INPUT_DIR),
                 '--Output-outputDir={0}'.format(outputDir),
