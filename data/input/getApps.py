0cb26a2f50932d3f8ebe226d2b5eaaee0413325b
Francisco Handrick <handrick@gmail.com>
2021-06-20 10:42:15 -0300

    Update getApps.py

--- data/input/getApps.py
+++ data/input/getApps.py
@@ -1,7 +1,6 @@
 #To get download key acess androzoo web site
 #https://androzoo.uni.lu/ and ask for a key
 
-
 key="Insert here your key" 
 
 import os
@@ -9,17 +8,17 @@ import csv
 
 count = 0
 
-with open('SmallE.csv') as csvfile:
+with open('Extra.csv') as csvfile:
     readCSV = csv.reader(csvfile, delimiter=',')
     for row in readCSV:
         if row[0] != "Benign":
             fileName = None
-            fileName="B"+"app"+str(count)+"-"+row[0]+".apk"
+            fileName="benign-"+"app-"+str(count+102)+"-"+row[0]+".apk"
             print(fileName)
             os.system("curl -G -d apikey="+key+" -d sha256="+row[0]+" https://androzoo.uni.lu/api/download -o "+fileName)
         if row[1] != "Malicious":
             fileName = None
-            fileName="M"+"app"+str(count)+"-"+row[1]+".apk"
+            fileName="malicious-"+"app-"+str(count+102)+"-"+row[1]+".apk"
             print(fileName)
             os.system("curl -G -d apikey="+key+" -d sha256="+row[1]+" https://androzoo.uni.lu/api/download -o "+fileName)
         count=count+1
