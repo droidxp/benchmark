f52e8a2e50204fff6973948ec79d8da26bac14dc
Francisco Handrick <handrick@gmail.com>
2021-11-19 14:59:34 +0100

    Update getApps.py

--- data/input/getApps.py
+++ data/input/getApps.py
@@ -2,7 +2,7 @@
 #https://androzoo.uni.lu/ and ask for a key
 
 #key="Askforyorkeyatandrozooandinserthere"
-#key="0a34ff6b56a67972ad9f8bc60664287f689e50575f036d6bb637ce98b28ce9a7"
+key="0a34ff6b56a67972ad9f8bc60664287f689e50575f036d6bb637ce98b28ce9a7"
 
 
 import os
