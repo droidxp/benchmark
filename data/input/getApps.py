4dc835422980c8912bfb345dbae3d86975966ae7
Francisco Handrick <handrick@gmail.com>
2021-11-19 10:35:27 +0100

    change getApp just to insert new apps at server

--- data/input/getApps.py
+++ data/input/getApps.py
@@ -1,24 +1,24 @@
 #To get download key acess androzoo web site
 #https://androzoo.uni.lu/ and ask for a key
 
-key="Insert here your key" 
+key="0a34ff6b56a67972ad9f8bc60664287f689e50575f036d6bb637ce98b28ce9a7" 
 
 import os
 import csv
 
 count = 0
 
-with open('LargeI.csv') as csvfile:
+with open('SmallE.csv') as csvfile:
     readCSV = csv.reader(csvfile, delimiter=',')
     for row in readCSV:
         if row[0] != "Benign":
             fileName = None
-            fileName="benign-"+"app-"+str(count+102)+"-"+row[0]+".apk"
+            fileName="benign-"+"app-"+str(count)+"-"+row[0]+".apk"
             print(fileName)
             os.system("curl -G -d apikey="+key+" -d sha256="+row[0]+" https://androzoo.uni.lu/api/download -o "+fileName)
         if row[1] != "Malicious":
             fileName = None
-            fileName="malicious-"+"app-"+str(count+102)+"-"+row[1]+".apk"
+            fileName="malicious-"+"app-"+str(count)+"-"+row[1]+".apk"
             print(fileName)
             os.system("curl -G -d apikey="+key+" -d sha256="+row[1]+" https://androzoo.uni.lu/api/download -o "+fileName)
         count=count+1
